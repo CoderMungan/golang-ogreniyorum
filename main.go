37181fc4fb570ab9ed1235b00267b50c14d76b49
CoderMungan <codermungan@gmail.com>
2023-11-12 17:22:18 +0300

    Değişkenler

--- main.go
+++ main.go
@@ -1,6 +1,24 @@
-package main
+package main //Ana Paket başlangıç noktası
 import "fmt"
+
+
 
 func main(){
 	fmt.Println("Hello World!")
+
+
+	// Değişkenler
+
+	// var name string
+	// var email string
+	// var age int
+
+	// Farklı esnek kullanımlar
+	var (
+		isim, email string = "Mehmet Halil", "halilmungan@gmail.com"
+		yas int = 25
+	)
+
+	fmt.Println("İsmi :",isim,"Email Adresi :",email,"Yaşı :",yas)
 }
+
