d046e0324f7d6337b0dbd8634e96965780b5602f
CoderMungan <codermungan@gmail.com>
2023-11-13 02:41:09 +0300

    First Step

--- main.go
+++ main.go
@@ -1,23 +1,22 @@
-package main //Ana Paket başlangıç noktası
+package main
 import "fmt"
-
 
 func main(){
 	fmt.Println("Hello World!")
-
-
-	// Değişkenler
-
-	// var name string
-	// var email string
-	// var age int
-
-	// Farklı esnek kullanımlar
-	var (
-		isim, email string = "Mehmet Halil", "halilmungan@gmail.com"
-		yas int = 25
-	)
-
-	fmt.Println("İsmi :",isim,"Email Adresi :",email,"Yaşı :",yas)
+	// Tekli yorum satırı
+	/* 
+		Satırları kapsayan yorum satırı!
+		@CoderMungan
+	*/
+	// Types
+	var name string
+	var car string = "Mercedes"
+	var price float64 = 15000.99
+	var isMarried bool = true
+	y := 2 // Bu değişkende ":=" operatörü sonradan değer alabileceğini gösterir. JS'de let var gibi.
+	// y string = "Naber" // Ama type ilk olarak ilk olduğundan hata döndürür aynı type'de değer almalıdır.
+	// ":=" Fonksiyon dışında kullanılamaz hata döndürür
+	fmt.Println(car,price,isMarried,y) // Mercedes 15000.99 true 2
+	name = "CoderMungan"
+	fmt.Println(name) // CoderMungan Çıktısı alınıyor
 }
-
